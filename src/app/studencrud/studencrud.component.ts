802ab74790e7a65c210ead23ad796e1bf10376d9
Kleyson Cabral <kleyson.cabral3@gmail.com>
2024-03-16 19:32:27 -0400

    feat: implementando frontend

---
+++ src/app/studencrud/studencrud.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studencrud',
+  standalone: true,
+  imports: [],
+  templateUrl: './studencrud.component.html',
+  styleUrl: './studencrud.component.scss'
+})
+export class StudencrudComponent {
+
+}
